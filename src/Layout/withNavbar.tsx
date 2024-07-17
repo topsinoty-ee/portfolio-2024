5c5bc83fd073068b36e981f79e2e237f87a19222
Oluwatobiloba Promise Temitope <oluwatobilobatemi05@gmail.com>
2024-07-17 15:09:30 +0300

    Added SEO

--- src/Layout/withNavbar.tsx
+++ src/Layout/withNavbar.tsx
@@ -3,14 +3,17 @@
 import { ComponentType } from 'react';
 import Navbar from '../components/Navbar';
 import PageTitle from '../components/PageTitle';
+import SEO from '../components/SEO';
 
 export const withNavbar = <P extends object>(
   WrappedPageComponent: ComponentType<P>,
-  title:string
+  title:string,
+  desc: string
 ) => {
   return (props: P) => {
     return (
       <>
+      <SEO title={title} description={desc} />
         <Navbar
           navlinks={[
             { src: '/', children: 'home' },
