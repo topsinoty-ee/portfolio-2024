099afa99b9ae408c18c8fba38b925f0a1e36f9b1
Oluwatobiloba Promise Temitope <oluwatobilobatemi05@gmail.com>
2024-07-18 00:51:29 +0300

    added projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,13 +1,16 @@
 /** @format */
 
 import { Tooltip } from '../components/Misc';
+import Project from '../components/Project';
+import Veric from '../assets/images/Softr-Veric.png';
+import DoyenHR from '../assets/images/DoyenHr-Hero.png';
 
-const Project = () => {
+const Projects = () => {
   return (
     <section
       tabIndex={0}
       id="#projects"
-      className="w-full flex justify-between h-[90vh] carousel-item snap-start snap-always">
+      className="w-full flex justify-between h-[90vh] carousel-item snap-start snap-always container">
       <div className="py-4 px-6 flex flex-col gap-5 w-full">
         <div className="flex flex-col gap-2">
           <h1 className="text-4xl font-semibold">My projects</h1>
@@ -20,14 +23,40 @@ const Project = () => {
           </p>
         </div>
 
-        <div className="carousel w-full bg-white h-full gap-4">
-          <div className="h-full bg-base-200 w-1/2 carousel-item"></div>
-          <div className="h-full bg-base-200 w-1/2 carousel-item"></div>
-          <div className="h-full bg-base-200 w-1/2 carousel-item"></div>
+        <div className="carousel w-full h-full gap-4">
+          <Project
+            src={Veric}
+            title="Veric"
+            description="Veric is a This was a collaborative project assigned from codesters.club with a company interface and a user interface. This is the user interface. It was made with Python (flask) and Tailwindcss + DaisyUI."
+            tags={[
+              'Python',
+              'Flask',
+              'TailwindCSS',
+              'DasiyUI',
+              'Airtable',
+              'Softr',
+              'Jinja2',
+            ]}
+            link="https://veric-cto.replit.app"
+            repo="https://github.com/topsinoty-ee/CTO"
+          />
+          <Project
+            src={DoyenHR}
+            title="DoyenHR"
+            description="An HR software application like Slack or Monday.com. Made with TypeScript + Tailwindcss + React"
+            tags={[
+              'TypeScript',
+              'React',
+              'TailwindCSS',
+              'BeaverDB',
+              'styled-components',
+            ]}
+            link="https://hr.doyenify.com"
+          />
         </div>
       </div>
     </section>
   );
 };
 
-export default Project;
+export default Projects;
